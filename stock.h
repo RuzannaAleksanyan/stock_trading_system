9b00ecd21431b0fff199af8dfc17fe6c9e636f6b
Roz Ale <110930300+RuzannaAleksanyan@users.noreply.github.com>
2023-10-24 03:32:57 +0400

    Create stock.h

---
+++ stock.h
@@ -0,0 +1,27 @@
+#ifndef STOCK
+#define STOCK
+
+#include <unordered_map>
+#include <string>
+#include <cstdlib>   
+#include <ctime>     
+
+class Stock {
+private:
+    std::string m_name;
+    double m_current_price;
+    int m_quantity;
+
+public:
+    Stock(const std::string& stock_name, double initial_price, int quantity = 0);
+
+    std::string get_stock_name() const;
+    double get_current_price() const;
+    int get_quantity() const;
+
+    void set_quantity(int new_quantity);
+    
+    void randomize_price();
+};
+
+#endif
